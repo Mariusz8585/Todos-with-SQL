f64f47de53954daaf1268d733063c03c5464d26a
Mariusz <mariusz-rakowski24@wp.pl>
2021-04-05 18:28:37 +0200

    Initial commit

---
+++ forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField
+
+class TodoForm(FlaskForm):
+    title = StringField('title')
+    description = TextAreaField('description')
+    done = BooleanField('done')
